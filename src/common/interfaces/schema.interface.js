0bd13e4beaceae6399822a70f17d77b5e71093df
Anph <anph@flomail.net>
2026-03-26 18:04:28 +0700

    remove compiled files

--- src/common/interfaces/schema.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SafetyLevel = void 0;
-var SafetyLevel;
-(function (SafetyLevel) {
-    SafetyLevel["SAFE"] = "SAFE";
-    SafetyLevel["WARNING"] = "WARNING";
-    SafetyLevel["CRITICAL"] = "CRITICAL";
-})(SafetyLevel || (exports.SafetyLevel = SafetyLevel = {}));
